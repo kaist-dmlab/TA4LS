a005b23925e9d4bb86c7d0214389feb605b932b4
Jihye Na <tnsrnr25@gmail.com>
2025-06-01 21:15:12 +0900

    Update sweep_params.py

--- configs/sweep_params.py
+++ configs/sweep_params.py
@@ -15,31 +15,11 @@ sweep_alg_hparams = {
             'domain_loss_wt':   {'distribution': 'uniform', 'min': 1e-2, 'max': 10},
         },
 
-        'AdvSKM': {
-            'learning_rate':    {'values': [1e-2, 5e-3, 1e-3, 5e-4]},
-            'src_cls_loss_wt':  {'distribution': 'uniform', 'min': 1e-1, 'max': 10},
-            'domain_loss_wt':   {'distribution': 'uniform', 'min': 1e-2, 'max': 10},
-        },
-
         'CoDATS': {
             'learning_rate':    {'values': [1e-2, 5e-3, 1e-3, 5e-4]},
             'src_cls_loss_wt':  {'distribution': 'uniform', 'min': 1e-1, 'max': 10},
             'domain_loss_wt':   {'distribution': 'uniform', 'min': 1e-2, 'max': 10},
         },
-
-        'CDAN': {
-            'learning_rate':    {'values': [1e-2, 5e-3, 1e-3, 5e-4]},
-            'src_cls_loss_wt':  {'distribution': 'uniform', 'min': 1e-1, 'max': 10},
-            'domain_loss_wt':   {'distribution': 'uniform', 'min': 1e-2, 'max': 10},
-            'cond_ent_wt':      {'distribution': 'uniform', 'min': 1e-2, 'max': 10},
-        },
-
-        'Deep_Coral': {
-            'learning_rate':    {'values': [1e-2, 5e-3, 1e-3, 5e-4]},
-            'src_cls_loss_wt':  {'distribution': 'uniform', 'min': 1e-1, 'max': 10},
-            'coral_wt':         {'distribution': 'uniform', 'min': 1e-1, 'max': 10},
-        },
-
         'DIRT': {
             'learning_rate':    {'values': [1e-2, 5e-3, 1e-3, 5e-4]},
             'src_cls_loss_wt':  {'distribution': 'uniform', 'min': 1e-1, 'max': 10},
@@ -47,19 +27,10 @@ sweep_alg_hparams = {
             'cond_ent_wt':      {'distribution': 'uniform', 'min': 1e-2, 'max': 10},
             'vat_loss_wt':      {'distribution': 'uniform', 'min': 1e-2, 'max': 10},
         },
-
         'HoMM': {
             'learning_rate':    {'values': [1e-2, 5e-3, 1e-3, 5e-4]},
             'src_cls_loss_wt':  {'distribution': 'uniform', 'min': 1e-1, 'max': 10},
             'hommd_wt':         {'distribution': 'uniform', 'min': 1e-2, 'max': 10},
-        },
-
-        'MMDA': {
-            'learning_rate':    {'values': [1e-2, 5e-3, 1e-3, 5e-4]},
-            'src_cls_loss_wt':  {'distribution': 'uniform', 'min': 1e-1, 'max': 10},
-            'coral_wt':         {'distribution': 'uniform', 'min': 1e-2, 'max': 10},
-            'cond_ent_wt':      {'distribution': 'uniform', 'min': 1e-2, 'max': 10},
-            'mmd_wt':           {'distribution': 'uniform', 'min': 1e-2, 'max': 10},
         },
 
         'DSAN': {
@@ -68,26 +39,11 @@ sweep_alg_hparams = {
             'mmd_wt':           {'distribution': 'uniform', 'min': 1e-2, 'max': 10},
         },
 
-        'DDC': {
-            'learning_rate':    {'values': [1e-2, 5e-3, 1e-3, 5e-4]},
-            'src_cls_loss_wt':  {'distribution': 'uniform', 'min': 1e-1, 'max': 10},
-            'mmd_wt':           {'distribution': 'uniform', 'min': 1e-2, 'max': 10},
-        },
-        
         'SASA': {
             'learning_rate':    {'values': [1e-2, 5e-3, 1e-3, 5e-4]},
             'src_cls_loss_wt':  {'distribution': 'uniform', 'min': 1e-1, 'max': 10},
             'domain_loss_wt':   {'distribution': 'uniform', 'min': 1e-2, 'max': 10},
         },
 
-        'CoTMix': {
-            'learning_rate':            {'values': [1e-2, 5e-3, 1e-3, 5e-4]},
-            'temporal_shift':           {'values': [5, 10, 15, 20, 30, 50]},
-            'src_cls_weight':           {'distribution': 'uniform', 'min': 1e-1, 'max': 1},
-            'mix_ratio':                {'distribution': 'uniform', 'min': 0.5, 'max': 0.99},
-            'src_supCon_weight':        {'distribution': 'uniform', 'min': 1e-3, 'max': 1},
-            'trg_cont_weight':          {'distribution': 'uniform', 'min': 1e-3, 'max': 1},
-            'trg_entropy_weight':       {'distribution': 'uniform', 'min': 1e-3, 'max': 1},
-        },
 }
 
